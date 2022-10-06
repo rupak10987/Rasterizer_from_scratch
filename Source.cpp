5db460a37908a5b634be0daf6eaf37dd3713a261
Rupak Saha <rupak10987@gmail.com>
2022-10-06 22:40:02 +0600

    basic_canvas

--- Source.cpp
+++ Source.cpp
@@ -8,5 +8,12 @@ int main()
     std::cout<<"hola amigos"<<std::endl;
     class Vec3 A(1,1,1);
     std::cout<<"DOT(A,A)= "<<A.DOT_PRODUCT(A,A)<<std::endl;
+    int window=initwindow(500,500,"RASTERIZER");
+    setcurrentwindow(window);
+    for(int i=-500;i<500;i++)
+        for(int j=-500;j<500;j++)
+            putpixel(i,j,COLOR(255,0,0));
+    getch();
+     closegraph();
     return 0;
 }
