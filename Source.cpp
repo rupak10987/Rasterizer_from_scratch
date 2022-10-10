9e407614d2cb532d56c40f1db2240b73f65320e3
Rupak Saha <rupak10987@gmail.com>
2022-10-10 22:13:23 +0600

    errors

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@
 #include"Light_Source.h"
 #include"Col.h"
 double Vals[500]={0};
-void ineterpolate(class Vec3 P,class Vec3 P1);
+void ineterpolate(double x0,double y0,double x1,double y1);
 void draw_line(class Vec3 P,class Vec3 P1,class Col col);
 class Vec3 normalized_to_screen_cord(int X,int Y);
 int Win_Width=500;
@@ -14,10 +14,12 @@ int main()
 {
     int win=initwindow(Win_Width,Win_Height,(const char*)"RASTERIZER");
     setcurrentwindow(win);
-    class Vec3 p(0,0,0);
-    class Vec3 p1(10,250,0);
-    class Col c(255,255,0);
+    class Vec3 p(-250,-250,0);
+    class Vec3 p1(0,0,0);
+    class Col c(255,255,255);
     draw_line(p,p1,c);
+    //p.y=50;
+    //draw_line(p,p1,c);
     getch();
     closegraph();
     return 0;
@@ -26,28 +28,54 @@ int main()
 void draw_line(class Vec3 P,class Vec3 P1,class Col col)
 {
 
-        /*double* ys=*/ineterpolate(P,P1);
+
+        //check if the function is horizontalish or verticalish
+        double dx=abs(P1.x-P.x);
+        double dy=abs(P1.y-P.y);
+        if(dx>dy)//horizontalish
+        {
+        if(P.x>P1.x)//always goes to right from left
+        draw_line(P1,P,col);
+        ineterpolate(P.x,P.y,P1.x,P1.y);
         for(int i=P.x;i<P1.x;i++)
         {
         class Vec3 screen_cord;
-        screen_cord=normalized_to_screen_cord(i,Vals[i-(int)P.x]);
+        screen_cord=normalized_to_screen_cord(i,Vals[i-(int)P.x+(Win_Width/2)]);
+std::cout<<screen_cord.x<<", "<<screen_cord.y<<std::endl;
         putpixel(screen_cord.x,screen_cord.y,COLOR(col.r,col.g,col.b));
         }
+        }
+        else//verticalish just changing dependent to independent & vice versa
+        {
+        if(P.y>P1.y)//always goes to up from down
+        draw_line(P1,P,col);
+        ineterpolate(P.y,P.x,P1.y,P1.x);
+        for(int i=P.y;i<P1.y;i++)
+        {
+        class Vec3 screen_cord;
+        screen_cord=normalized_to_screen_cord(i,Vals[i-(int)P.y+(Win_Width/2)]);
+        std::cout<<screen_cord.x<<", "<<screen_cord.y<<std::endl;
+        putpixel(screen_cord.y,screen_cord.x,COLOR(col.r,col.g,col.b));
+        }
+        }
+
 };
 
-void ineterpolate(class Vec3 P,class Vec3 P1)
+void ineterpolate(double x0,double y0,double x1,double y1)
 {
 
-if(P.x==P1.x)
+if(x0==x1)
 {
-    Vals[int(P.x)]={P.y};
+    Vals[int(x0)+(Win_Width/2)]={y0};
 }
-double m=P.clac_xy_slope(P.Direction_Vec(P1,P));
-double y=P.y;
-for(int i=P.x;i<=P1.x;i++)
+double m=abs(y1-y0)/abs(x1-x0);
+
+double y=y0;
+for(int i=x0;i<=x1;i++)
 {
-   Vals[i]=y;
-    y+=m;
+
+   Vals[i+250]=y;
+    y=y+m;
 }
 }
 
